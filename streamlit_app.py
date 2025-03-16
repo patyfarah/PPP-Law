684e3c496662e8ed6a906789b1808115ba30d730
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-03-16 20:07:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ client = OpenAI(api_key="sk-...DyQA")
 
 # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
-"Upload a document (.txt or .md)", type=("txt", "md")
+"Upload a document (.pdf)", type=("pdf")
 )
 
 # Ask the user for a question via `st.text_area`.
@@ -21,24 +21,23 @@ question = st.text_area(
 placeholder="Can you give me a short summary?",
 disabled=not uploaded_file,
 )
-
 if uploaded_file and question:
 
-# Process the uploaded file and question.
-document = uploaded_file.read().decode()
-messages = [
-    {
-        "role": "user",
-        "content": f"Here's a document: {document} \n\n---\n\n {question}",
-    }
-]
-
-# Generate an answer using the OpenAI API.
-stream = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=messages,
-    stream=True,
-)
-
-# Stream the response to the app using `st.write_stream`.
-st.write_stream(stream)
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
+    
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True,
+    )
+    
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
